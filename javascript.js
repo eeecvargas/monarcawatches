bbda33c4f8fe0bb4bd70c469ed50a0ce15b4a795
eeecvargas <163078591+eeecvargas@users.noreply.github.com>
2024-03-18 22:31:53 -0500

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,34 @@
+let usuarionombre = prompt("Hola! cuál es tu nombre?")
+
+
+document.write("Welcome "+usuarionombre)
+
+
+
+
+
+
+//console.log("Soy JS y estoy dentro de html")
+//alert()
+//otra forma de salida de datos
+//docwrite  y alert no se puede ejecutar en replit ya que ambas necesitan de html
+
+//document.write("SPORTING CRISTAL");
+
+//let usuario_nombre = prompt("Cuál es tu nombre?");
+//console.log("Bienvenida "+ usuario_nombre)
+//document.write("Bienvenida " + usuario_nombre)
+
+//if(x>y){
+// document.write("x es mayor a y")
+
+
+//}
+
+//let horaactual=13;
+//if (horaactual>12){
+//    document.write("Buenas tardes :)")
+//} else {
+//   if document.write("Buenas npches :)")
+//}
+
